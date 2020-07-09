dd61609b6350907ca92ab40161e635a1fd3dfdf8
Laete Meireles <laetemn@gmail.com>
2020-07-08 21:57:14 -0300

    .suo

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,15 +29,124 @@
         private void InitializeComponent()
         {
             this.menuStripPrincipal = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.capasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exibirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ferramentasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.atualizarListaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportarListaDeJogosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportarParaCSVToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportarParaTXTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sincronizarEBaixarCapasSomente3DToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sincronizarEBaixarCapastodasAsCapasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.configuraçõesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStripPrincipal.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStripPrincipal
             // 
+            this.menuStripPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem,
+            this.capasToolStripMenuItem,
+            this.exibirToolStripMenuItem,
+            this.ferramentasToolStripMenuItem});
             this.menuStripPrincipal.Location = new System.Drawing.Point(0, 0);
             this.menuStripPrincipal.Name = "menuStripPrincipal";
             this.menuStripPrincipal.Size = new System.Drawing.Size(1328, 24);
             this.menuStripPrincipal.TabIndex = 0;
             this.menuStripPrincipal.Text = "menuStripPrincipal";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.atualizarListaToolStripMenuItem,
+            this.exportarListaDeJogosToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.sairToolStripMenuItem});
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.arquivoToolStripMenuItem.Text = "Arquivo";
+            // 
+            // capasToolStripMenuItem
+            // 
+            this.capasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sincronizarEBaixarCapasSomente3DToolStripMenuItem,
+            this.sincronizarEBaixarCapastodasAsCapasToolStripMenuItem});
+            this.capasToolStripMenuItem.Name = "capasToolStripMenuItem";
+            this.capasToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.capasToolStripMenuItem.Text = "Capas";
+            // 
+            // exibirToolStripMenuItem
+            // 
+            this.exibirToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.configuraçõesToolStripMenuItem});
+            this.exibirToolStripMenuItem.Name = "exibirToolStripMenuItem";
+            this.exibirToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.exibirToolStripMenuItem.Text = "Exibir";
+            // 
+            // ferramentasToolStripMenuItem
+            // 
+            this.ferramentasToolStripMenuItem.Name = "ferramentasToolStripMenuItem";
+            this.ferramentasToolStripMenuItem.Size = new System.Drawing.Size(84, 20);
+            this.ferramentasToolStripMenuItem.Text = "Ferramentas";
+            // 
+            // atualizarListaToolStripMenuItem
+            // 
+            this.atualizarListaToolStripMenuItem.Name = "atualizarListaToolStripMenuItem";
+            this.atualizarListaToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+            this.atualizarListaToolStripMenuItem.Text = "Atualizar lista";
+            // 
+            // exportarListaDeJogosToolStripMenuItem
+            // 
+            this.exportarListaDeJogosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exportarParaCSVToolStripMenuItem,
+            this.exportarParaTXTToolStripMenuItem});
+            this.exportarListaDeJogosToolStripMenuItem.Name = "exportarListaDeJogosToolStripMenuItem";
+            this.exportarListaDeJogosToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+            this.exportarListaDeJogosToolStripMenuItem.Text = "Exportar lista de jogos";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(187, 6);
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            // 
+            // exportarParaCSVToolStripMenuItem
+            // 
+            this.exportarParaCSVToolStripMenuItem.Name = "exportarParaCSVToolStripMenuItem";
+            this.exportarParaCSVToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exportarParaCSVToolStripMenuItem.Text = "Exportar para CSV";
+            // 
+            // exportarParaTXTToolStripMenuItem
+            // 
+            this.exportarParaTXTToolStripMenuItem.Name = "exportarParaTXTToolStripMenuItem";
+            this.exportarParaTXTToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exportarParaTXTToolStripMenuItem.Text = "Exportar para TXT";
+            // 
+            // sincronizarEBaixarCapasSomente3DToolStripMenuItem
+            // 
+            this.sincronizarEBaixarCapasSomente3DToolStripMenuItem.Name = "sincronizarEBaixarCapasSomente3DToolStripMenuItem";
+            this.sincronizarEBaixarCapasSomente3DToolStripMenuItem.Size = new System.Drawing.Size(324, 22);
+            this.sincronizarEBaixarCapasSomente3DToolStripMenuItem.Text = "Sincronizar e baixar capas (somente Disco e 3D)";
+            // 
+            // sincronizarEBaixarCapastodasAsCapasToolStripMenuItem
+            // 
+            this.sincronizarEBaixarCapastodasAsCapasToolStripMenuItem.Name = "sincronizarEBaixarCapastodasAsCapasToolStripMenuItem";
+            this.sincronizarEBaixarCapastodasAsCapasToolStripMenuItem.Size = new System.Drawing.Size(324, 22);
+            this.sincronizarEBaixarCapastodasAsCapasToolStripMenuItem.Text = "Sincronizar e baixar capas (todas as capas)";
+            // 
+            // configuraçõesToolStripMenuItem
+            // 
+            this.configuraçõesToolStripMenuItem.Name = "configuraçõesToolStripMenuItem";
+            this.configuraçõesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.configuraçõesToolStripMenuItem.Text = "Configurações...";
             // 
             // Form1
             // 
@@ -47,7 +156,10 @@
             this.Controls.Add(this.menuStripPrincipal);
             this.MainMenuStrip = this.menuStripPrincipal;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "GameCube Backup Manager";
+            this.menuStripPrincipal.ResumeLayout(false);
+            this.menuStripPrincipal.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,6 +168,19 @@
         #endregion
 
         private System.Windows.Forms.MenuStrip menuStripPrincipal;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem atualizarListaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportarListaDeJogosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportarParaCSVToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportarParaTXTToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem capasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sincronizarEBaixarCapasSomente3DToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sincronizarEBaixarCapastodasAsCapasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exibirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem configuraçõesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ferramentasToolStripMenuItem;
     }
 }
 
