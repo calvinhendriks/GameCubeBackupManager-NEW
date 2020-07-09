2e27c61a3e21893398400798ea64622ec52140ae
Laete Meireles <laetemn@gmail.com>
2020-07-08 21:26:24 -0300

    .suo
    .suo

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStripPrincipal = new System.Windows.Forms.MenuStrip();
             this.SuspendLayout();
+            // 
+            // menuStripPrincipal
+            // 
+            this.menuStripPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.menuStripPrincipal.Name = "menuStripPrincipal";
+            this.menuStripPrincipal.Size = new System.Drawing.Size(1328, 24);
+            this.menuStripPrincipal.TabIndex = 0;
+            this.menuStripPrincipal.Text = "menuStripPrincipal";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1328, 594);
+            this.Controls.Add(this.menuStripPrincipal);
+            this.MainMenuStrip = this.menuStripPrincipal;
             this.Name = "Form1";
             this.Text = "GameCube Backup Manager";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStripPrincipal;
     }
 }
 
