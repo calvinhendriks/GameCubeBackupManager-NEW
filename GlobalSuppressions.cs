a1061413318a7ed566c8728237004a054801ca44
Laete Meireles <laetemn@gmail.com>
2020-07-30 15:45:13 -0300

    Create GlobalSuppressions.cs

---
+++ GlobalSuppressions.cs
@@ -0,0 +1,43 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.GCDiscInfo.getMaxFSTSize~System.String")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.GCDiscInfo.getGameName~System.String")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.formPrincipal.tsmiAbout_Click(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.formPrincipal.tsmiConfig_Click(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.formPrincipal.tsmiPrincipalExit_Click(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.GCDiscInfo.convertAsciiToHex(System.String)~System.String")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.GCDiscInfo.convertHexToAscii(System.String)~System.String")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.GCDiscInfo.getConsoleId~System.String")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.GCDiscInfo.getCountryCode~System.String")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.GCDiscInfo.getDebugMonitorLoadAddress~System.String")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.GCDiscInfo.getDebugMonitorOffset~System.String")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.GCDiscInfo.getDiscId~System.String")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.GCDiscInfo.getDVDMagicWord~System.String")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.GCDiscInfo.getFSTOffset~System.String")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.GCDiscInfo.getFSTSize~System.String")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.GCDiscInfo.getGameCode~System.String")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.GCDiscInfo.getHexStringFromFile(System.Int32,System.Int32)~System.String")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.GCDiscInfo.getMainExecutableDOLOffset~System.String")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.GCDiscInfo.getMakerCode~System.String")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.GCDiscInfo.getVersion~System.String")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.GCDiscInfo.setConsoleID(System.String)~System.Boolean")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.GCDiscInfo.setCountryCode(System.String)~System.Boolean")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.GCDiscInfo.setDebugMonitorLoadAddress(System.String)~System.Boolean")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.GCDiscInfo.setDebugMonitorOffset(System.String)~System.Boolean")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.GCDiscInfo.setDiscId(System.String)~System.Boolean")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.GCDiscInfo.setDVDMagicWord(System.String)~System.Boolean")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.GCDiscInfo.setFSTOffset(System.String)~System.Boolean")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.GCDiscInfo.setGameCode(System.String)~System.Boolean")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.GCDiscInfo.setGameName(System.String)~System.Boolean")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.GCDiscInfo.setMainExecutableDOLOffset(System.String)~System.Boolean")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.GCDiscInfo.setMakerCode(System.String)~System.Boolean")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.GCDiscInfo.setMaxFSTSize(System.String)~System.Boolean")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.GCDiscInfo.setVersion(System.String)~System.Boolean")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.GCDiscInfo.writeByteArrayToFile(System.String,System.Int32,System.Byte[])~System.Boolean")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~M:GameCubeBackupManager.IniFile.corrigeFileName(System.String)~System.String")]
+[assembly: SuppressMessage("Style", "IDE1006:Estilos de Nomenclatura", Justification = "<Pendente>", Scope = "member", Target = "~P:GameCubeBackupManager.IniFile.fileName")]
